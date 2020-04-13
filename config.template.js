dddf494fb94bc7d5e036d05a50d5b3e03d4aa6d1
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-04-13 10:10:06 -0400

    updated config.template

--- config.template.js
+++ config.template.js
@@ -1,3 +1,12 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+    emailAddress: 'email address to be used to send messages',
+    scopes: 'array of strings containing permissions needed from google',
+    spreadsheetId: 'id of google sheet',
+    tableRange: '<sheetName>!<cellRange ex. A2:B>',
+    dateRange: {
+        startDate: 'Date or moment object',
+        endDate: 'Date or moment object'
+    }
+};
