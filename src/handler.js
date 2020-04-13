c0f2daed46a017a431be2ff77549c0cec0432802
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-04-13 09:47:35 -0400

    added note to handler

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,12 @@ const getApiList = require('./lib/google');
 const sendOmnisendEmail = require('./lib/mail');
 let { startDate, endDate } = require('../config').dateRange;
 
-exports.handler = async function (event) {
+// handler function for lambda which will expect an array of recipients
+// in the event param (configured in AWS cloudwatch events). this is
+// the main program flow as it will use the omnisend lib to get data, then
+// parse into a csv string which will be attached to the email sent by the
+// mail lib to all recipients from event param
+exports.handler = async function(event) {
     startDate = startDate.format('YYYY-MM-DD');
     endDate = endDate.format('YYYY-MM-DD');
 
@@ -18,7 +23,9 @@ exports.handler = async function (event) {
         ]
     });
 
-    const csvString = `${csvStringifier.getHeaderString()}${csvStringifier.stringifyRecords(data)}`;
+    const csvString = `${csvStringifier.getHeaderString()}${csvStringifier.stringifyRecords(
+        data
+    )}`;
 
     for (const recipient of event.recipients) {
         await sendOmnisendEmail({
@@ -33,5 +40,4 @@ exports.handler = async function (event) {
             ]
         }).catch(err => console.error(err));
     }
-}
-
+};
